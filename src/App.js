975c35f0d2bc7102f770f336968df67ff821fd8d
sigristarisa <arisasigrist.ch@gmail.com>
2022-05-12 17:08:12 +0200

    made header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import Header from "./components/Header/Header";
+import "./index.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="title">Yeastie Boys</h1>
+      <div>
+        <button>Calculator</button>
+        <button>Recipes</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
