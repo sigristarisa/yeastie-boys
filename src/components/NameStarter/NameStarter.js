58f4dca1c5e893107bca41637606379c593da70c
sigristarisa <arisasigrist.ch@gmail.com>
2022-05-21 13:26:24 +0200

    added styling for NameStarter

--- src/components/NameStarter/NameStarter.js
+++ src/components/NameStarter/NameStarter.js
@@ -1,9 +1,24 @@
+import "./NameStarter.css";
+
 const NameStarter = () => {
   return (
-    <main>
-      <h1>What is your starter's name?</h1>
-      <input type="text" name="starter-name" />
-      <input type="submit" />
+    <main className="name-starter">
+      <div>
+        <h1>
+          Hello, there! <br></br> Name your sourdough starter:
+        </h1>
+        <div className="name-starter-input-wrapper">
+          <input
+            className="name-starter-input"
+            type="text"
+            name="starter-name"
+          />
+          <div className="name-starter-button-wrapper">
+            <input type="submit" value="Go" />
+            <input type="submit" value="Skip" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
